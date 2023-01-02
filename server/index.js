0656da75818cc4e5d6b1a150f0af1b5052696417
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2023-01-02 15:05:05 +0900

    ユーザー新規作成 & パスワード暗号化

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const CryptoJS = require("crypto-js");
+const User = require("./src/v1/models/user");
+
 const app = express();
 const PORT = 3000;
 require('dotenv').config();
@@ -12,6 +15,22 @@ try {
 } catch (error) {
   console.log(error)
 }
+
+// ユーザー新規登録API
+app.post("/register", async (req, res) => {
+  const password = req.body.password;
+
+  try {
+    // パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    // ユーザーの新規作成
+    const user = await User.create(req.body);
+  } catch {
+
+  }
+});
+
+// ユーザーログインAPI
 
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中・・・")
