d4e7f1b2ae96416265f54261c84bf24bbb1aa5dc
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2023-01-03 15:12:36 +0900

    新規作成 & ログイン画面雛形

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthLayout from './components/layout/AuthLayout';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App"></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
